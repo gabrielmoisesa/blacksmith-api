591053cbd4b5d091abd6b1a8b36ba9f4588b7e74
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-16 17:48:47 -0300

    req 1: products service test

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,28 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import ProductModel from '../../../src/database/models/product.model';
+import ProductService from '../../../src/services/product.service';
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
 
-});
+  describe('Create', function() {
+    it('Should create a product and return the created product', async function () {
+      const productBody = {
+        name: 'Product 1',
+        price: '10 peças de ouro',
+        orderId: 1,
+      };
+      const mockCreatedProduct = ProductModel.build({id: 1, ...productBody});
+      sinon.stub(ProductModel, 'create').resolves(mockCreatedProduct);
+
+      const productReturn = await ProductService.create(productBody);
+
+      expect(productReturn).to.be.deep.equal({
+        id: 1,
+        name: productBody.name,
+        price: productBody.price,
+      })
+    });
+  })
+}); 
