c1a8447d5a25270678b6141fbf31779970c638c9
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-17 00:34:24 -0300

    test: product service get all test

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,18 +1,29 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import ProductModel from '../../../src/database/models/product.model';
-import ProductService from '../../../src/services/product.service';
+import productService from '../../../src/services/product.service';
 import productMock from '../../mocks/product.mock';
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
+
+  describe('Get All', function() {
+    it('Should return all products', async function () {
+      sinon.stub(ProductModel as any, 'findAll').resolves(productMock.products);
+
+      const productServiceReturn = await productService.getAll();
+
+      expect(productServiceReturn.status).to.be.equal('OK');
+      expect(productServiceReturn.data).to.be.equal(productMock.getAllResponse.data);
+    });
+  })
 
   describe('Create', function() {
     it('Should create a product and return the created product', async function () {
       const mockCreatedProduct = ProductModel.build({id: 1, ...productMock.body});
       sinon.stub(ProductModel, 'create').resolves(mockCreatedProduct);
 
-      const productServiceReturn = await ProductService.create(productMock.body);
+      const productServiceReturn = await productService.create(productMock.body);
 
       expect(productServiceReturn).to.be.deep.equal(productMock.createdResponse)
     });
