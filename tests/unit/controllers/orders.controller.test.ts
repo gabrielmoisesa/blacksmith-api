6448d75cb8293a250a24f2c792e68b2a0652359b
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-17 20:42:04 -0300

    test: orders controller

--- tests/unit/controllers/orders.controller.test.ts
+++ tests/unit/controllers/orders.controller.test.ts
@@ -1,13 +1,17 @@
 import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import orderMock from '../../mocks/order.mock';
+import orderService from '../../../src/services/order.service';
+import orderController from '../../../src/controllers/order.controller'
 
 chai.use(sinonChai);
 
 describe('OrdersController', function () {
   const req = {} as Request;
   const res = {} as Response;
+  const next = {} as NextFunction;
 
   beforeEach(function () {
     res.status = sinon.stub().returns(res);
@@ -15,4 +19,14 @@ describe('OrdersController', function () {
     sinon.restore();
   });
 
+  describe('Get All', function() {
+    it('Should return all orders with status 200', async function () {
+      sinon.stub(orderService, 'getAll').resolves({ status: 'OK', data: orderMock.findAllResponse });
+
+      await orderController.getAll(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(orderMock.findAllResponse);
+    })
+  })
 });
