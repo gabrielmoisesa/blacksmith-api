ab50005d0cba765739d978d511160e1ea02f5ab1
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-17 18:04:43 -0300

    req 4: add a any message to login schema body

--- src/middlewares/schemas/login.schema.ts
+++ src/middlewares/schemas/login.schema.ts
@@ -3,6 +3,8 @@ import joi from 'joi';
 const body = joi.object({
   username: joi.string().required(),
   password: joi.string().required(),
+}).messages({
+  'any.required': '"username" and "password" are required',
 });
 
 export default {
