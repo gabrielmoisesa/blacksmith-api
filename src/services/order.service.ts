aa61842c6486348a49b241e5f43f4d4be33ec571
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-16 21:47:48 -0300

    req 3: change and create order types and format orders get all response to includes product ids

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,9 +1,27 @@
-import OrderModel, { OrderSequelizeModel } from '../database/models/order.model';
+import OrderModel from '../database/models/order.model';
+import ProductModel from '../database/models/product.model';
+import { Order, OrderResponse } from '../types/Order';
 import { ServiceResponse } from '../types/ServiceResponse';
 
-const getAll = async (): Promise<ServiceResponse<OrderSequelizeModel[]>> => {
-  const orders = await OrderModel.findAll();
-  return { status: 'OK', data: orders };
+const getAll = async (): Promise<ServiceResponse<OrderResponse[]>> => {
+  const orders = await OrderModel.findAll({
+    include: [
+      {
+        model: ProductModel,
+        as: 'productIds',
+        foreignKey: 'orderId',
+        attributes: ['id'],
+      },
+    ],
+  });
+
+  const ordersFormatted = orders.map((order) => {
+    const orderFormatted = order.toJSON() as Order;
+    const productIds = (orderFormatted.productIds || []).map((product) => product.id);
+    return { ...orderFormatted, productIds };
+  });
+
+  return { status: 'OK', data: ordersFormatted };
 };
 
 export default {
--- src/types/Order.ts
+++ src/types/Order.ts
@@ -1,5 +1,11 @@
 export type Order = {
   id: number;
   userId: number;
-  productId?: number;
+  productIds?: { id: number }[];
+};
+
+export type OrderResponse = {
+  id: number;
+  userId: number;
+  productIds?: number[];
 };
