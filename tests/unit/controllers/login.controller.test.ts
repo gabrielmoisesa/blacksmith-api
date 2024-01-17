48dbb579a48704dfae11d8aaef398cbf3f6e6e10
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-17 19:19:12 -0300

    test: login controller

--- tests/unit/controllers/login.controller.test.ts
+++ tests/unit/controllers/login.controller.test.ts
@@ -1,13 +1,16 @@
 import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import loginService from '../../../src/services/login.service';
+import loginController from '../../../src/controllers/login.controller'
 
 chai.use(sinonChai);
 
 describe('LoginController', function () {
   const req = {} as Request;
   const res = {} as Response;
+  const next = {} as NextFunction;
 
   beforeEach(function () {
     res.status = sinon.stub().returns(res);
@@ -15,4 +18,15 @@ describe('LoginController', function () {
     sinon.restore();
   });
 
+  describe('Login', function() {
+    it('Should return the token with status 200', async function () {
+      req.body = { username: 'username', password: 'password' };
+      sinon.stub(loginService, 'login').resolves({ status: 'OK', data: { token: 'valid-token' } })
+
+      await loginController.login(req, res, next);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith({ token: 'valid-token' });
+    })
+  })
 });
