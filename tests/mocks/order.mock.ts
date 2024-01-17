a38561135e614ad566b4ca215874682d71d19514
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-17 20:36:16 -0300

    test: orders service test

---
+++ tests/mocks/order.mock.ts
@@ -0,0 +1,37 @@
+const orders = [
+  {
+    id: 1,
+    userId: 1,
+  },
+  {
+    id: 2,
+    userId: 2,
+  },
+  {
+    id: 3,
+    userId: 3,
+  },
+];
+
+const findAllResponse = [
+  {
+    id: 1,
+    userId: 1,
+    productIds: []
+  },
+  {
+    id: 2,
+    userId: 2,
+    productIds: []
+  },
+  {
+    id: 3,
+    userId: 3,
+    productIds: []
+  },
+]
+
+export default {
+  orders,
+  findAllResponse,
+}
--- tests/unit/services/orders.service.test.ts
+++ tests/unit/services/orders.service.test.ts
@@ -1,7 +1,23 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import OrderModel from '../../../src/database/models/order.model';
+import orderMock from '../../mocks/order.mock';
+import orderService from '../../../src/services/order.service'
 
-describe('OrdersService', function () {
-  beforeEach(function () { sinon.restore(); });
+describe("OrdersService", function () {
+  beforeEach(function () {
+    sinon.restore();
+  });
 
+  describe("Get All", function () {
+    it("Should return all orders", async function () {
+      const mockOrders = OrderModel.bulkBuild(orderMock.orders);
+      sinon.stub(OrderModel, 'findAll').resolves(mockOrders);
+
+      const orderServiceReturn = await orderService.getAll();
+
+      expect(orderServiceReturn.status).to.be.equal('OK');
+      expect(orderServiceReturn.data).to.be.deep.equal(orderMock.findAllResponse);
+    });
+  });
 });
