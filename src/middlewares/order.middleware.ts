f5d50298a1b2eae0249a64dfd67bcebfba476244
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-18 03:12:35 -0300

    req 6: changes order create schema message when array is empty

--- src/middlewares/order.middleware.ts
+++ src/middlewares/order.middleware.ts
@@ -4,7 +4,7 @@ import orderSchema from './schemas/order.schema';
 const validateBody: RequestHandler = (req, res, next) => {
   const { error } = orderSchema.body.validate(req.body);
   if (error) {
-    const statusCode = error.message.includes('must be') ? 422 : 400;
+    const statusCode = error.message.includes('must') ? 422 : 400;
     return res.status(statusCode).json({ message: error.message });
   }
   next();
--- src/middlewares/schemas/order.schema.ts
+++ src/middlewares/schemas/order.schema.ts
@@ -1,9 +1,11 @@
 import joi from 'joi';
 
 const body = joi.object({
-  productIds: joi.array().items(joi.number().required()).required().messages({
-    'number.base': '"productIds" must include only numbers',
-  }),
+  productIds: joi.array().items(joi.number()).min(1).required()
+    .messages({
+      'number.base': '"productIds" must include only numbers',
+      'array.min': '"productIds" must include only numbers',
+    }),
   userId: joi.number().required(),
 });
 
