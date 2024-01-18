2b5bef0c366a53e77a0dafe0ac4770933e6a6b90
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-18 03:01:22 -0300

    req 6: changes order body schema message when productids array is not a number array

--- src/middlewares/schemas/order.schema.ts
+++ src/middlewares/schemas/order.schema.ts
@@ -1,7 +1,9 @@
 import joi from 'joi';
 
 const body = joi.object({
-  productIds: joi.array().items(joi.number()).required(),
+  productIds: joi.array().items(joi.number().required()).required().messages({
+    'number.base': '"productIds" must include only numbers',
+  }),
   userId: joi.number().required(),
 });
 
