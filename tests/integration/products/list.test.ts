a751c60ba0fab92fd514a81389f426b2a5890637
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-24 23:46:25 -0300

    test: create get products integration test & add prettier single quote rule

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,23 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import ProductModel from '../../../src/database/models/product.model';
+import productMock from '../../mocks/product.mock';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
-describe('GET /products', function () { 
-  beforeEach(function () { sinon.restore(); });
+describe('GET /products', function () {
+  beforeEach(function () {
+    sinon.restore();
+  });
 
+  it('should return all products with status 200', async function () {
+    const mockProducts = ProductModel.bulkBuild([productMock.products]);
+    sinon.stub(ProductModel, 'findAll').resolves(mockProducts);
+
+    const response = await chai.request(app).get('/products');
+
+    expect(response.status).to.be.equal(200);
+  });
 });
