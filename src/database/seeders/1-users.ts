3df50ba998215d9b0d4e9bdb0d3cb8be2d369a22
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2024-01-26 00:25:37 -0300

    refactor: translate database seeders

--- src/database/seeders/1-users.ts
+++ src/database/seeders/1-users.ts
@@ -5,29 +5,33 @@ const SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS || 10;
 
 export default {
   up(queryInterface: QueryInterface) {
-    return queryInterface.bulkInsert('users', [
-      {
-        username: 'Hagar',
-        vocation: 'Guerreiro',
-        level: 10,
-        password: bcrypt.hashSync('terrível', SALT_ROUNDS),
-      },
-      {
-        username: 'Eddie',
-        vocation: 'Guerreiro',
-        level: 8,
-        password: bcrypt.hashSync('sortudo', SALT_ROUNDS),
-      },
-      {
-        username: 'Helga',
-        vocation: 'Curandeira',
-        level: 9,
-        password: bcrypt.hashSync('valquíria', SALT_ROUNDS),
-      },
-    ], {});
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          username: 'Hagar',
+          vocation: 'Warrior',
+          level: 10,
+          password: bcrypt.hashSync('terrible', SALT_ROUNDS),
+        },
+        {
+          username: 'Eddie',
+          vocation: 'Warrior',
+          level: 8,
+          password: bcrypt.hashSync('lucky', SALT_ROUNDS),
+        },
+        {
+          username: 'Helga',
+          vocation: 'Healer',
+          level: 9,
+          password: bcrypt.hashSync('valkyrie', SALT_ROUNDS),
+        },
+      ],
+      {}
+    );
   },
-  
+
   down(queryInterface: QueryInterface) {
     return queryInterface.bulkDelete('users', {});
-  }
+  },
 };
--- src/database/seeders/3-products.ts
+++ src/database/seeders/3-products.ts
@@ -2,36 +2,40 @@ import { QueryInterface } from 'sequelize';
 
 export default {
   up(queryInterface: QueryInterface) {
-    return queryInterface.bulkInsert('products', [
-      {
-        name: 'Excalibur',
-        price: '10 peças de ouro',
-        order_id: 1
-      },
-      {
-        name: 'Espada Justiceira',
-        price: '20 peças de ouro',
-        order_id: 1
-      },
-      {
-        name: 'Lira de Orfeu',
-        price: '1 peça de ouro',
-        order_id: 2
-      },
-      {
-        name: 'Armadura de Aquiles',
-        price: '1 peça de ouro',
-        order_id: 2
-      },
-      {
-        name: 'Harpa de Dagda',
-        price: '15 peças de ouro',
-        order_id: 3
-      },
-    ], {});
+    return queryInterface.bulkInsert(
+      'products',
+      [
+        {
+          name: 'Excalibur',
+          price: '10 gold pieces',
+          order_id: 1,
+        },
+        {
+          name: 'Sword of Omens',
+          price: '20 gold pieces',
+          order_id: 1,
+        },
+        {
+          name: "Orpheus' lyre",
+          price: '1 gold piece',
+          order_id: 2,
+        },
+        {
+          name: 'Armor of Achilles',
+          price: '1 gold piece',
+          order_id: 2,
+        },
+        {
+          name: "Dagda's harp",
+          price: '15 gold pieces',
+          order_id: 3,
+        },
+      ],
+      {}
+    );
   },
-  
+
   down(queryInterface: QueryInterface) {
     return queryInterface.bulkDelete('products', {});
-  }
+  },
 };
